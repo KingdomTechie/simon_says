70c16a89badc4e6dc388f6520f0b0d1e34bd620e
Keith Powell <keith@tapcart.co>
2021-09-09 15:03:47 -0400

    Connected sound to random color genereated

--- game.js
+++ game.js
@@ -4,14 +4,17 @@ const gamePattern = [];
 let randomChosenColor = "";
 
 function nextSequence() {
-
+    
     let randomNumber = Math.floor(Math.random() * 4)
-
+    
     randomChosenColor = buttonOfColors[randomNumber]
-
+    
     gamePattern.push(randomChosenColor)
-
+    
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+    let audio = new Audio (`sounds/${randomChosenColor}.mp3`)
+    audio.play()
+
 
     return randomChosenColor
 
