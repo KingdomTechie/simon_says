a026a9b5eaab74109777ca7d205381df1aa84646
Keith Powell <keith@tapcart.co>
2021-09-10 09:38:34 -0400

    Added comments to code

--- game.js
+++ game.js
@@ -11,9 +11,13 @@ function nextSequence() {
     
     randomChosenColor = buttonOfColors[randomNumber]
     
+    // Stored log of all colors the computer randomly generates
     gamePattern.push(randomChosenColor)
     
+    // This code causes the blinking animation when function is called
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    // This code creates the sound corresponding to the color
     let audio = new Audio (`sounds/${randomChosenColor}.mp3`)
     audio.play()
 
@@ -23,9 +27,28 @@ function nextSequence() {
 
 // User clicks on a button and records event.  Also plays sound based on color pressed
 $(".btn").on("click", function (event) {
+
     let userChosenColor = event.currentTarget.id
+
+    let button = event.currentTarget
+
     userClickedPattern.push(userChosenColor)
+
+    // when a button is clicked, the corresponding sound will play
     let audio = new Audio (`sounds/${userChosenColor}.mp3`)
     audio.play()
-    console.log(userClickedPattern)
+
+    animatePress(userChosenColor)
+    
 })
+function animatePress (currentColor) {
+
+    $("#"+currentColor).addClass("pressed")
+
+    setInterval(function () {
+    
+            $("#"+currentColor).removeClass("pressed")
+
+
+    }, 100)
+}
